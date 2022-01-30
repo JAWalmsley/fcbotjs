bedcef52a50e954476968be8ba777d48ac1c7345
Jack Walmsley <jackawalmsley@gmail.com>
2022-01-30 03:04:48 -0500

    Finished colour change command

--- main.js
+++ main.js
@@ -15,26 +15,44 @@ client.on('interactionCreate', async interaction => {
         await interaction.reply('Pong!');
     } else if (interaction.commandName === 'wumpus') {
         await interaction.reply({files: ['wumpus/' + interaction.options.getString('emotion')]})
-    } else if (interaction.commandName === 'colour') {
-        let hexRE = new RegExp("^#(?:[0-9a-fA-F]{3}){1,2}$");
-        if (!hexRE.test(interaction.options.getString('colour'))) {
-            await interaction.reply('That\'s not a colour!');
-            return;
-        }
-        await interaction.reply('cool');
-        let desiredColour = interaction.options.getString('colour').toLowerCase();
-
-        let newRoleInfo = {
-            name: desiredColour,
-            color: desiredColour,
-            reason: 'User desired new role for setting colour'
-        }
-        interaction.guild.roles.create(newRoleInfo).then((newrole) => {
-                interaction.member.roles.add(newRole);
+    } else {
+        if (interaction.commandName === 'colour') {
+            let hexRE = new RegExp("^#(?:[0-9a-fA-F]{3}){1,2}$");
+            if (!hexRE.test(interaction.options.getString('colour'))) {
+                await interaction.reply('That\'s not a colour!');
+                return;
             }
-        )
+            let desiredColour = interaction.options.getString('colour').toUpperCase();
+            // If a three-character hexcolor, make six-character
+            if (desiredColour.length === 4) {
+                desiredColour = '#' + desiredColour.replace('#', '').split('').map(function (hex) {
+                    return hex + hex;
+                }).join('');
+            }
 
+            let newRoleInfo = {
+                name: desiredColour,
+                color: desiredColour,
+                reason: 'User desired new role for setting colour'
+            }
 
+            let roles = interaction.member.roles.cache.map(role => role);
+            roles.forEach(async role => {
+                if (role.name.startsWith('#')) {
+                    await interaction.member.roles.remove(role);
+                    if (role.members.size == 0) {
+                        interaction.guild.roles.delete(role);
+                    }
+                }
+            });
+
+            interaction.guild.roles.create(newRoleInfo).then((newRole) => {
+                    interaction.member.roles.add(newRole);
+                }
+            );
+            await interaction.reply({content: 'Now you look all ' + desiredColour + '!', ephemeral: true});
+
+        }
     }
 });
 
