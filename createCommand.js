1b3f488fec29ab417247a946e8b30928ad602927
Jack Walmsley <jackawalmsley@gmail.com>
2022-01-30 01:55:23 -0500

    HTTP request for creating required commands

---
+++ createCommand.js
@@ -0,0 +1,164 @@
+const {REST} = require('@discordjs/rest');
+const {Routes} = require('discord-api-types/v9');
+
+const commands =
+    [{
+        name: 'ping',
+        description: 'Replies with Pong!'
+    },
+        {
+            name: 'wumpus',
+            description: 'Sends a Wumpus sticker',
+            options: [
+                {
+                    name: 'emotion',
+                    description: 'The emotion Wumpus should display',
+                    type: 3,
+                    required: true,
+                    choices: [
+                        {
+                            name:'angry',
+                            value:'angry.gif'
+                        },
+
+                        {
+                            name:'aww',
+                            value:'aww.gif'
+                        },
+
+                        {
+                            name:'blush',
+                            value:'blush.gif'
+                        },
+
+                        {
+                            name:'cry',
+                            value:'cry.gif'
+                        },
+
+                        {
+                            name:'curious',
+                            value:'curious.gif'
+                        },
+
+                        {
+                            name:'dance',
+                            value:'dance.gif'
+                        },
+
+                        {
+                            name:'evil',
+                            value:'evil.gif'
+                        },
+
+                        {
+                            name:'frustrated',
+                            value:'frustrated.gif'
+                        },
+
+                        {
+                            name:'hug',
+                            value:'hug.gif'
+                        },
+
+                        {
+                            name:'hungry',
+                            value:'hungry.gif'
+                        },
+
+                        {
+                            name:'joy',
+                            value:'joy.gif'
+                        },
+
+                        {
+                            name:'love',
+                            value:'love.gif'
+                        },
+
+                        {
+                            name:'nervous',
+                            value:'nervous.gif'
+                        },
+
+                        {
+                            name:'no',
+                            value:'no.gif'
+                        },
+
+                        {
+                            name:'ok',
+                            value:'ok.gif'
+                        },
+
+                        {
+                            name:'relief',
+                            value:'relief.gif'
+                        },
+
+                        {
+                            name:'sad',
+                            value:'sad.gif'
+                        },
+
+                        {
+                            name:'scared',
+                            value:'scared.gif'
+                        },
+
+                        {
+                            name:'sick',
+                            value:'sick.gif'
+                        },
+
+                        {
+                            name:'sleepy',
+                            value:'sleepy.gif'
+                        },
+
+                        {
+                            name:'smile',
+                            value:'smile.gif'
+                        },
+
+                        {
+                            name:'surprise',
+                            value:'surprise.gif'
+                        },
+
+                        {
+                            name:'thanks',
+                            value:'thanks.gif'
+                        },
+
+                        {
+                            name:'thinking',
+                            value:'thinking.gif'
+                        },
+
+                        {
+                            name:'wave',
+                            value:'wave.gif'
+                        }
+                    ]
+                }
+            ]
+        }];
+
+const rest = new REST({version: '9'}).setToken('MzA4MDIwNTIyNjQwNDc0MTE0.WQUgdw.bN_NgRUxnEd1ziSLzt25jFMDwzw');
+
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(
+            // Routes.applicationGuildCommands("308020522640474114", "424671611057078283"),
+            Routes.applicationCommands("308020522640474114"),
+            {body: commands},
+        );
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
