97bb9bd50431e96754fb1ac1f6408cdfbb060c69
Jack Walmsley <jackawalmsley@gmail.com>
2024-02-12 13:27:23 -0500

    Added minecraft server stuff

--- createCommand.js
+++ createCommand.js
@@ -1,5 +1,5 @@
-const {REST} = require('@discordjs/rest');
-const {Routes} = require('discord-api-types/v9');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
 
 const config = require('./config.json')
 
@@ -8,161 +8,165 @@ const commands =
         name: 'ping',
         description: 'Replies with Pong!'
     },
-        {
-            name: 'wumpus',
-            description: 'Sends a Wumpus sticker',
-            options: [
-                {
-                    name: 'emotion',
-                    description: 'The emotion Wumpus should display',
-                    type: 3,
-                    required: true,
-                    choices: [
-                        {
-                            name:'angry',
-                            value:'angry.gif'
-                        },
+    {
+        name: 'wumpus',
+        description: 'Sends a Wumpus sticker',
+        options: [
+            {
+                name: 'emotion',
+                description: 'The emotion Wumpus should display',
+                type: 3,
+                required: true,
+                choices: [
+                    {
+                        name: 'angry',
+                        value: 'angry.gif'
+                    },
 
-                        {
-                            name:'aww',
-                            value:'aww.gif'
-                        },
+                    {
+                        name: 'aww',
+                        value: 'aww.gif'
+                    },
 
-                        {
-                            name:'blush',
-                            value:'blush.gif'
-                        },
+                    {
+                        name: 'blush',
+                        value: 'blush.gif'
+                    },
 
-                        {
-                            name:'cry',
-                            value:'cry.gif'
-                        },
+                    {
+                        name: 'cry',
+                        value: 'cry.gif'
+                    },
 
-                        {
-                            name:'curious',
-                            value:'curious.gif'
-                        },
+                    {
+                        name: 'curious',
+                        value: 'curious.gif'
+                    },
 
-                        {
-                            name:'dance',
-                            value:'dance.gif'
-                        },
+                    {
+                        name: 'dance',
+                        value: 'dance.gif'
+                    },
 
-                        {
-                            name:'evil',
-                            value:'evil.gif'
-                        },
+                    {
+                        name: 'evil',
+                        value: 'evil.gif'
+                    },
 
-                        {
-                            name:'frustrated',
-                            value:'frustrated.gif'
-                        },
+                    {
+                        name: 'frustrated',
+                        value: 'frustrated.gif'
+                    },
 
-                        {
-                            name:'hug',
-                            value:'hug.gif'
-                        },
+                    {
+                        name: 'hug',
+                        value: 'hug.gif'
+                    },
 
-                        {
-                            name:'confetti',
-                            value:'confetti.gif'
-                        },
+                    {
+                        name: 'confetti',
+                        value: 'confetti.gif'
+                    },
 
-                        {
-                            name:'joy',
-                            value:'joy.gif'
-                        },
+                    {
+                        name: 'joy',
+                        value: 'joy.gif'
+                    },
 
-                        {
-                            name:'love',
-                            value:'love.gif'
-                        },
+                    {
+                        name: 'love',
+                        value: 'love.gif'
+                    },
 
-                        {
-                            name:'nervous',
-                            value:'nervous.gif'
-                        },
+                    {
+                        name: 'nervous',
+                        value: 'nervous.gif'
+                    },
 
-                        {
-                            name:'no',
-                            value:'no.gif'
-                        },
+                    {
+                        name: 'no',
+                        value: 'no.gif'
+                    },
 
-                        {
-                            name:'ok',
-                            value:'ok.gif'
-                        },
+                    {
+                        name: 'ok',
+                        value: 'ok.gif'
+                    },
 
-                        {
-                            name:'relief',
-                            value:'relief.gif'
-                        },
+                    {
+                        name: 'relief',
+                        value: 'relief.gif'
+                    },
 
-                        {
-                            name:'sad',
-                            value:'sad.gif'
-                        },
+                    {
+                        name: 'sad',
+                        value: 'sad.gif'
+                    },
 
-                        {
-                            name:'scared',
-                            value:'scared.gif'
-                        },
+                    {
+                        name: 'scared',
+                        value: 'scared.gif'
+                    },
 
-                        {
-                            name:'sick',
-                            value:'sick.gif'
-                        },
+                    {
+                        name: 'sick',
+                        value: 'sick.gif'
+                    },
 
-                        {
-                            name:'sleepy',
-                            value:'sleepy.gif'
-                        },
+                    {
+                        name: 'sleepy',
+                        value: 'sleepy.gif'
+                    },
 
-                        {
-                            name:'smile',
-                            value:'smile.gif'
-                        },
+                    {
+                        name: 'smile',
+                        value: 'smile.gif'
+                    },
 
-                        {
-                            name:'surprise',
-                            value:'surprise.gif'
-                        },
+                    {
+                        name: 'surprise',
+                        value: 'surprise.gif'
+                    },
 
-                        {
-                            name:'thanks',
-                            value:'thanks.gif'
-                        },
+                    {
+                        name: 'thanks',
+                        value: 'thanks.gif'
+                    },
 
-                        {
-                            name:'thinking',
-                            value:'thinking.gif'
-                        },
+                    {
+                        name: 'thinking',
+                        value: 'thinking.gif'
+                    },
 
-                        {
-                            name:'wave',
-                            value:'wave.gif'
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            name: 'colour',
-            description: 'Sets user colour',
-            options: [
-                {
-                    name: 'colour',
-                    description: 'The colour you want',
-                    type: 3,
-                    required: true
-                }
+                    {
+                        name: 'wave',
+                        value: 'wave.gif'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        name: 'colour',
+        description: 'Sets user colour',
+        options: [
+            {
+                name: 'colour',
+                description: 'The colour you want',
+                type: 3,
+                required: true
+            }
 
-            ]
+        ]
 
-        }
+    },
+    {
+        name: 'online',
+        description: 'Lists the players on the minecraft server',
+    }
     ];
 
-const rest = new REST({version: '9'}).setToken(config.token);
+const rest = new REST({ version: '9' }).setToken(config.token);
 
 (async () => {
     try {
@@ -170,12 +174,12 @@ const rest = new REST({version: '9'}).setToken(config.token);
 
         await rest.put(
             Routes.applicationGuildCommands("308020522640474114", "424671611057078283"),
-            {body: commands},
+            { body: commands },
         );
 
         await rest.put(
             Routes.applicationCommands("308020522640474114"),
-            {body: commands},
+            { body: commands },
         );
 
         console.log('Successfully reloaded application (/) commands.');
--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-const {Client, Intents} = require('discord.js');
-const client = new Client({intents: [Intents.FLAGS.GUILDS]});
+const { Client, Intents, ActivityType } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 const config = require('./config.json')
 
-const fs = require('fs')
+const fs = require('fs');
 
+async function mconline() {
+    let online = await fetch('https://api.mcsrvstat.us/3/mc.jackwalmsley.com');
+    let playernames = (await online.json()).players.list.map(p => p.name);
+    client.user.setActivity(`${playernames.length} online | mc.jackwalmsley.com\npog\npog`, { type: "PLAYING" })
+    return playernames;
+}
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    mconline();
 });
 
 client.on('interactionCreate', async interaction => {
@@ -16,46 +23,62 @@ client.on('interactionCreate', async interaction => {
     if (interaction.commandName === 'ping') {
         await interaction.reply('Pong!');
     } else if (interaction.commandName === 'wumpus') {
-        await interaction.reply({files: ['wumpus/' + interaction.options.getString('emotion')]})
-    } else {
-        if (interaction.commandName === 'colour') {
-            let hexRE = new RegExp("^#(?:[0-9a-fA-F]{3}){1,2}$");
-            if (!hexRE.test(interaction.options.getString('colour'))) {
-                await interaction.reply('That\'s not a colour!');
-                return;
-            }
-            let desiredColour = interaction.options.getString('colour').toUpperCase();
-            // If a three-character hexcolor, make six-character
-            if (desiredColour.length === 4) {
-                desiredColour = '#' + desiredColour.replace('#', '').split('').map(function (hex) {
-                    return hex + hex;
-                }).join('');
-            }
-
-            let newRoleInfo = {
-                name: desiredColour,
-                color: desiredColour,
-                reason: 'User desired new role for setting colour'
-            }
-
-            let roles = interaction.member.roles.cache.map(role => role);
-            roles.forEach(async role => {
-                if (role.name.startsWith('#')) {
-                    await interaction.member.roles.remove(role);
-                    if (role.members.size == 0) {
-                        interaction.guild.roles.delete(role);
-                    }
-                }
-            });
-
-            interaction.guild.roles.create(newRoleInfo).then((newRole) => {
-                    interaction.member.roles.add(newRole);
-                }
-            );
-            await interaction.reply({content: 'Now you look all ' + desiredColour + '!', ephemeral: true});
-
+        await interaction.reply({ files: ['wumpus/' + interaction.options.getString('emotion')] })
+    } else if (interaction.commandName === 'colour') {
+        let hexRE = new RegExp("^#(?:[0-9a-fA-F]{3}){1,2}$");
+        if (!hexRE.test(interaction.options.getString('colour'))) {
+            await interaction.reply('That\'s not a colour!');
+            return;
         }
+        let desiredColour = interaction.options.getString('colour').toUpperCase();
+        // If a three-character hexcolor, make six-character
+        if (desiredColour.length === 4) {
+            desiredColour = '#' + desiredColour.replace('#', '').split('').map(function (hex) {
+                return hex + hex;
+            }).join('');
+        }
+
+        let newRoleInfo = {
+            name: desiredColour,
+            color: desiredColour,
+            reason: 'User desired new role for setting colour'
+        }
+
+        let roles = interaction.member.roles.cache.map(role => role);
+        roles.forEach(async role => {
+            if (role.name.startsWith('#')) {
+                await interaction.member.roles.remove(role);
+                if (role.members.size == 0) {
+                    interaction.guild.roles.delete(role);
+                }
+            }
+        });
+
+        interaction.guild.roles.create(newRoleInfo).then((newRole) => {
+            interaction.member.roles.add(newRole);
+        }
+        );
+        await interaction.reply({ content: 'Now you look all ' + desiredColour + '!', ephemeral: true });
+    }
+    else if (interaction.commandName === 'online') {
+        console.log("got a thing!");
+        let players = await mconline();
+        // await interaction.reply({content: `${(players).map(p => p + "\n")}`});
+        let msgcontent = {
+            embeds:
+                [{
+                    "title": `mc.jackwalmsley.com`,
+                    "description": `${players.length} players online`,
+                    "color": 0x00FFFF,
+                    fields: players.map(function(p) {
+                        return {name: p, value: " "}
+                    })
+                }]
+        }
+        await interaction.reply(msgcontent);
     }
 });
+
+
 
 client.login(config.token);
